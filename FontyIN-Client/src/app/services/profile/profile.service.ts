573a2d4bebb3f2b27a57bfd27df4c64ca3d11e5c
Rawan Abou Dehn <r.aboudehn@student.fontys.nl>
2020-10-14 22:56:19 +0200

    Change port number in the profile service

--- FontyIN-Client/src/app/services/profile/profile.service.ts
+++ FontyIN-Client/src/app/services/profile/profile.service.ts
@@ -9,7 +9,7 @@ export class ProfileService {
   constructor(private httpClient: HttpClient) { }
 
   public getProfile(){
-    return this.httpClient.get('http://localhost:9099/users/1/profiles/1')
+    return this.httpClient.get('http://localhost:9090/users/1/profiles/1')
   }
   public getExperienceById(){
     return this.httpClient.get('http://localhost:9099/users/1/profiles/1/experiences')
