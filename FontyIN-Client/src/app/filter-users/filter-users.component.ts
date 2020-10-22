558d34524ad77d2a5f1d2fd524f9a19dea3c9187
Alayoubi,Ranim R <r.alayoubi@student.fontys.nl>
2020-10-22 19:49:58 +0200

    Filter by start study year is working and data is illustrated ;)

--- FontyIN-Client/src/app/filter-users/filter-users.component.ts
+++ FontyIN-Client/src/app/filter-users/filter-users.component.ts
@@ -14,32 +14,32 @@ export class FilterUsersComponent implements OnInit {
 
 
   years: Object[] = [
-    {value: '1995-0', viewValue: '1995'}, 
-    {value: '1996-1', viewValue: '1996'},
-    {value: '1997-2', viewValue: '1997'},
-    {value: '1998-3', viewValue: '1998'},
-    {value: '1999-4', viewValue: '1999'},
-    {value: '2000-5', viewValue: '2000'},
-    {value: '2001-6', viewValue: '2001'},
-    {value: '2002-7', viewValue: '2002'},
-    {value: '2003-8', viewValue: '2003'},
-    {value: '2004-9', viewValue: '2004'},
-    {value: '2005-10', viewValue: '2005'},
-    {value: '2006-11', viewValue: '2006'},
-    {value: '2007-12', viewValue: '2007'},
-    {value: '2008-13', viewValue: '2008'},
-    {value: '2009-14', viewValue: '2009'},
-    {value: '2010-15', viewValue: '2010'},
-    {value: '2011-16', viewValue: '2011'},
-    {value: '2012-17', viewValue: '2012'},
-    {value: '2013-18', viewValue: '2013'},
-    {value: '2014-19', viewValue: '2014'},
-    {value: '2015-20', viewValue: '2015'},
-    {value: '2016-21', viewValue: '2016'},
-    {value: '2017-22', viewValue: '2017'},
-    {value: '2018-23', viewValue: '2018'},
-    {value: '2019-24', viewValue: '2019'},
-    {value: '2020-25', viewValue: '2020'}
+    {value: '1995', viewValue: '1995'}, 
+    {value: '1996', viewValue: '1996'},
+    {value: '1997', viewValue: '1997'},
+    {value: '1998', viewValue: '1998'},
+    {value: '1999', viewValue: '1999'},
+    {value: '2000', viewValue: '2000'},
+    {value: '2001', viewValue: '2001'},
+    {value: '2002', viewValue: '2002'},
+    {value: '2003', viewValue: '2003'},
+    {value: '2004', viewValue: '2004'},
+    {value: '2005', viewValue: '2005'},
+    {value: '2006', viewValue: '2006'},
+    {value: '2007', viewValue: '2007'},
+    {value: '2008', viewValue: '2008'},
+    {value: '2009', viewValue: '2009'},
+    {value: '2010', viewValue: '2010'},
+    {value: '2011', viewValue: '2011'},
+    {value: '2012', viewValue: '2012'},
+    {value: '2013', viewValue: '2013'},
+    {value: '2014', viewValue: '2014'},
+    {value: '2015', viewValue: '2015'},
+    {value: '2016', viewValue: '2016'},
+    {value: '2017', viewValue: '2017'},
+    {value: '2018', viewValue: '2018'},
+    {value: '2019', viewValue: '2019'},
+    {value: '2020', viewValue: '2020'}
   ];
 
   departments: Object[] = [
@@ -120,8 +120,15 @@ export class FilterUsersComponent implements OnInit {
     this.filterService.filterUsersByStartStudyYear(year).subscribe((data)=>
     {
      this.users=<User[]>data;
-      console.log(this.users);     
+      console.log(this.users);  
+      console.log(this.realm);   
     });
+  }
+
+  real: string;
+
+  modoe(){
+    return this.real;
   }
 
 }
