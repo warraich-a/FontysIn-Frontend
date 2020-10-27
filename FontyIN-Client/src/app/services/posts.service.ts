11ee59e81e48989095ee9e95e96195b2e0928f73
i439426 <i439426@fhict.nl>
2020-10-27 10:42:12 +0100

    Fixed post serv

--- FontyIN-Client/src/app/services/posts.service.ts
+++ FontyIN-Client/src/app/services/posts.service.ts
@@ -10,39 +10,39 @@ export class PostsService {
   constructor(private httpClient: HttpClient) { }
 
   public getPosts(){
-   return this.httpClient.get('http://localhost:9099/posts/user/'+'1');
+   return this.httpClient.get('http://localhost:9090/posts/user/'+'1');
   }
 
   public getPost(id){
-    return this.httpClient.get('http://localhost:9099/posts/'+id);
+    return this.httpClient.get('http://localhost:9090/posts/'+id);
    }
 
   public newPost(data){
-    return this.httpClient.post('http://localhost:9099/posts', data).toPromise().then(data => {
+    return this.httpClient.post('http://localhost:9090/posts', data).toPromise().then(data => {
       console.log(data);
     });
     
   }
 
   public updatePost(data,id){
-    return this.httpClient.put('http://localhost:9099/posts/'+id, data).toPromise().then(data => {
+    return this.httpClient.put('http://localhost:9090/posts/'+id, data).toPromise().then(data => {
       console.log(data);
     });
     
   }
 
   public deletePost(id){
-    return this.httpClient.delete('http://localhost:9099/posts/'+id).toPromise().then(data => {
+    return this.httpClient.delete('http://localhost:9090/posts/'+id).toPromise().then(data => {
       console.log(data);
     });
   }
 
   public getCommentsByPostId(id){
-    return this.httpClient.get('http://localhost:9099/comments/post/'+id);
+    return this.httpClient.get('http://localhost:9090/comments/post/'+id);
   }
 
   public newComment(data){
-    return this.httpClient.post('http://localhost:9099/comments', data).toPromise().then(data => {
+    return this.httpClient.post('http://localhost:9090/comments', data).toPromise().then(data => {
       console.log(data);
     });
     
