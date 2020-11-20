f56a0be23b03105457e37c6bae2074a412828490
Alayoubi,Ranim R <r.alayoubi@student.fontys.nl>
2020-11-20 16:50:22 +0100

    Delete conversation is implemented fully ....

--- FontyIN-Client/src/app/start-conversation/start-conversation.component.ts
+++ FontyIN-Client/src/app/start-conversation/start-conversation.component.ts
@@ -55,33 +55,19 @@ export class StartConversationComponent implements OnInit {
     this.dialogRef.close();
   }
 
-  getConversation() {
-		this.messageService.get(this.id)
-			.subscribe((data) => {
-				this.conversation = <Conversation>data;
+  // getConversation() {
+	// 	this.messageService.get(this.id)
+	// 		.subscribe((data) => {
+	// 			this.conversation = <Conversation>data;
 
-				// Get users
-				this.currentUser = (this.conversation.messages[0].receiver.id == this.loggedInUser) ? this.conversation.messages[0].receiver : this.conversation.messages[0].sender;
-				this.friend = (this.conversation.messages[0].receiver.id != this.loggedInUser) ? this.conversation.messages[0].receiver : this.conversation.messages[0].sender;
-			})
-	}
+	// 			// Get users
+	// 			this.currentUser = (this.conversation.messages[0].receiver.id == this.loggedInUser) ? this.conversation.messages[0].receiver : this.conversation.messages[0].sender;
+	// 			this.friend = (this.conversation.messages[0].receiver.id != this.loggedInUser) ? this.conversation.messages[0].receiver : this.conversation.messages[0].sender;
+	// 		})
+	// }
 
   // Send message
 	submit(f) {
-		let newMessage = {
-			conversationId: this.conversation.id,
-			sender: this.currentUser,
-			receiver: this.friend,
-			content: f.value.message
-		}
-
-		this.messageService.create(newMessage)
-			.subscribe(() => {
-				this.getConversation();
-
-				f.reset();
-          this.scrollable.nativeElement.scrollTop = this.scrollable.nativeElement.scrollHeight;
-      });
       
       this.Close();
 	}
